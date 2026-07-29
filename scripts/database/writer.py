e585891c53dd4ecdc367355a2f8cbe87b6eba402
Carlos Michelutti <carlos.stz590@gmail.com>
2026-07-29 11:36:40 -0300

    feat
    feat: Create file to modularize the write in the database.

---
+++ scripts/database/writer.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+from scripts.database.session import engine
+
+
+def write_dataframe_to_table(
+    dataframe: pd.DataFrame,
+    table_name: str,
+    schema: str = 'public',
+    if_exists: str = 'replace',
+) -> int:
+
+    insert_rows = dataframe.to_sql(
+        name=table_name,
+        con=engine,
+        schema=schema,
+        if_exists=if_exists,
+        index=False
+    )
+
+    print(f'DataFrame written {insert_rows} to table: {schema}.{table_name} (if_exists={if_exists})')
+    return insert_rows
